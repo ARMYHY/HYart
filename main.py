bbe3836d476a3bab904d2c26b2a698966dea46bc
ARMYHY <7happyjackie7@gmail.com>
2023-01-08 15:50:22 +0900

    add painter
    그림 그리는 부분 추가

--- main.py
+++ main.py
@@ -1,42 +1,5 @@
 '''
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QTextEdit)
-
-
-class MyApp(QWidget):
-
-    def __init__(self):
-        super().__init__()
-        self.initUI()
-
-    def initUI(self):
-        grid = QGridLayout()
-        self.setLayout(grid)
-
-        grid.addWidget(QLabel('Title:'), 0, 0)
-        grid.addWidget(QLabel('Author:'), 1, 0)
-        grid.addWidget(QLabel('Review:'), 2, 0)
-        grid.addWidget(QLabel('1:'), 3, 0)
-        grid.addWidget(QLabel('2:'), 4, 0)
-
-        grid.addWidget(QLineEdit(), 0, 1)
-        grid.addWidget(QLineEdit(), 1, 1)
-        grid.addWidget(QTextEdit(), 2, 1)
-        grid.addWidget(QLineEdit(), 3, 1)
-        grid.addWidget(QLineEdit(), 4, 1)
-
-        self.setWindowTitle('QGridLayout')
-        self.setGeometry(300, 300, 300, 200)
-        self.show()
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = MyApp()
-    sys.exit(app.exec_())
-'''
-
-import sys
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 
@@ -121,6 +84,7 @@ if __name__ == '__main__':
     sys.exit(app.exec_())
 
 '''
+'''
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp
 from PyQt5.QtGui import QIcon
@@ -138,10 +102,15 @@ class MyApp(QMainWindow):
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
 
-        saveAction = QAction(QIcon('save.png'), 'Save', self)
-        saveAction.setShortcut('Ctrl+S')
-        saveAction.setStatusTip('Save all')
-        saveAction.triggered.connect(qApp.quit)
+        homeAction = QAction(QIcon('save.png'), 'HOME', self)
+        homeAction.setShortcut('Ctrl+H')
+        homeAction.setStatusTip('Home application')
+        homeAction.triggered.connect(qApp.quit)
+
+        homeAction = QAction(QIcon('save.png'), 'HOME', self)
+        homeAction.setShortcut('Ctrl+H')
+        homeAction.setStatusTip('Home application')
+        homeAction.triggered.connect(qApp.quit)
 
         self.statusBar()
 
@@ -150,9 +119,18 @@ class MyApp(QMainWindow):
         filemenu = menubar.addMenu('&File')
         filemenu.addAction(exitAction)
 
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        filemenu = menubar.addMenu('&Home')
+        filemenu.addAction(homeAction)
+
         self.setWindowTitle('Menubar')
         self.setGeometry(300, 300, 300, 200)
         self.show()
+
+        # self.setWindowTitle('Menubar')
+        # self.setGeometry(300, 300, 300, 200)
+        # self.show()
 
 
 if __name__ == '__main__':
@@ -160,3 +138,99 @@ if __name__ == '__main__':
     ex = MyApp()
     sys.exit(app.exec_())
 '''
+
+
+'''
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        pixmap = QPixmap('landscape.jpg')
+
+        self.lbl_img = QLabel()
+        self.lbl_img.setPixmap(QtGui.QPixmap(400, 300))
+        lbl_size = QLabel('Width: '+str(pixmap.width())+', Height: '+str(pixmap.height()))
+        lbl_size.setAlignment(Qt.AlignCenter)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.lbl_img)
+        vbox.addWidget(lbl_size)
+        self.setLayout(vbox)
+        self.show()
+        self.draw_something()
+        self.show()
+
+    def draw_something(self):
+        painter = QtGui.QPainter(self.lbl_img.pixmap())
+        painter.drawLine(10, 10, 300, 200)
+        painter.end()
+        return
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    sys.exit(app.exec_())
+'''
+
+import sys
+
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+
+
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        pixmap = QPixmap()
+
+        lbl_img = QLabel()
+        lbl_img.setPixmap(pixmap)
+        lbl_size = QLabel('Width: '+str(pixmap.width())+', Height: '+str(pixmap.height()))
+        lbl_size.setAlignment(Qt.AlignCenter)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(lbl_img)
+        vbox.addWidget(lbl_size)
+        self.setLayout(vbox)
+
+        self.setWindowTitle('QPixmap')
+        self.move(300, 300)
+        self.show()
+
+    def paintEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        # self.draw_point(qp,e)
+        qp.end()
+
+    def draw_point(self, qp,e):
+        qp.drawPoint(e.x(), e.y())
+    def mouseMoveEvent(self, e):
+        qp = QPainter()
+        qp.begin(self)
+        self.draw_point(qp,e)
+        qp.end()
+        # qp.update()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
+
+
+

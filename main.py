147897ce12795c524cb8f0bebe01d2493e0bf9a2
ARMYHY <7happyjackie7@gmail.com>
2023-02-25 18:53:49 +0900

    마우스 x좌표,y좌표 나오게 만들기

--- main.py
+++ main.py
@@ -1,47 +1,28 @@
-'''
+
 import sys
 from PyQt5.QtWidgets import*
 from PyQt5.QtGui import*
 from PyQt5.QtCore import*
-
-
 class MyApp(QWidget):
-
     def __init__(self):
         super().__init__()
         self.initUI()
-
     def initUI(self):
         self.setWindowTitle('Points')
         self.show()
-        self.centralwidget = QWidget()
-        self.screen = QLabel(self.centralwidget)
-        self.screen.setPixmap(QPixmap("HOME.png"))
 
 
+    def mousePressEvent(self, event):
+        mouse_pt = "Mouse Point : sx={0},sy={1}".format(event.x(), event.y())
 
-        # 마우스 드레그 이벤트
-    def mouseMoveEvent(self, event):
-            # event.x(),y() : 마우스의 절대좌표 값
-        self.draw_Line(event.x(), event.y())
-
-        # 마우스 릴리즈 이벤트
     def mouseReleaseEvent(self, event):
-            # event.x(),y() : 마우스의 절대좌표 값
-        self.draw_Line(event.x(), event.y())
+        mouse_pt2 = "Mouse Point : ex={0},ey={1}".format(event.x(), event.y())
+        self.draw_Line(event.sx{0}, event.sy{1}, event.ex{2}, event.ey{3})
 
-    def draw_Line(self, x, y):
-        self.img = QPixmap("HOME2.png")
-        qp = QPainter(self.img)
-        qp.begin(self)
+    def draw_Line(self,event.sx{0}, event.sy{1}, event.ex{2}, event.ey{3}):
         qp.setPen(QPen(Qt.black, 10, Qt.SolidLine))
-        qp.drawLine(400, 300, x, y)
+        qp.drawLine(moust_pt,mouse_pt2)
         qp.end()
-
-
-
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
@@ -68,11 +49,19 @@ class MyApp(QMainWindow):
 
 
 
-    def mouseMoveEvent(self, event):
-        mouse_pt = "Mouse Point : x={0},y={0}".format(event.x(), event.y())
+    def mousePressEvent(self, event):
+        mouse_pt = "Mouse Point : sx={0},sy={1}".format(event.x(), event.y())
         self.status_bar.showMessage(mouse_pt)
+    def mouseReleaseEvent(self, event):
+        mouse_pt2 = "Mouse Point : ex={0},ey={1}".format(event.x(), event.y())
+        self.status_bar.showMessage(mouse_pt2)
+
+
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MyApp()
     sys.exit(app.exec_())
+'''
